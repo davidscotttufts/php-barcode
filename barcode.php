23c5df2c479c9069e25b97aca86598f4a30b323a
David Tufts <david.tufts@rocketwood.com>
2016-04-01 14:47:21 -0400

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -2,14 +2,24 @@
 
 /*
  *  Author:  David S. Tufts
- *  Company: Rocketwood.LLC
- *	  www.rocketwood.com
+ *  Company: N/A
+ *	  davidscotttufts.com
  *  Date:	05/25/2003
  *  Usage:
  *	  <img src="/barcode.php?text=testing" alt="testing" />
  */
-	
-function barcode($filepath="",$text="0",$size="20",$orientation="horizontal",$code_type="code128") {
+
+// For demonstration purposes, get pararameters that are passed in through $_GET or set to the default value
+$text = (isset($_GET["text"])?$_GET["text"]:"0");
+$size = (isset($_GET["size"])?$_GET["size"]:"20");
+$orientation = (isset($_GET["orientation"])?$_GET["orientation"]:"horizontal");
+$code_type = (isset($_GET["codetype"])?$_GET["codetype"]:"code128");
+$filepath = (isset($_GET["filepath"])?$_GET["filepath"]:"");
+
+// This function call can copied into your project and can be made from anywhere in your code
+barcode( $filepath, $text, $size, $orientation, $code_type );
+
+function barcode( $filepath="", $text="0", $size="20", $orientation="horizontal", $code_type="code128" ) {
 	$code_string = "";
 
 	// Translate the $text into barcode the correct $code_type
@@ -103,8 +113,9 @@ function barcode($filepath="",$text="0",$size="20",$orientation="horizontal",$co
 			imagefilledrectangle( $image, 0, $location, $img_width, $cur_size, ($position % 2 == 0 ? $white : $black) );
 		$location = $cur_size;
 	}
+	
 	// Draw barcode to the screen or save in a file
-	if($filepath=="") {
+	if ( $filepath=="" ) {
 		header ('Content-type: image/png');
 		imagepng($image);
 		imagedestroy($image);
